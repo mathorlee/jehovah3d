db79af28d985cfc3f21025a67d9498a7f8617850
mathorlee.li <mathorlee.li@fuwo.com>
2017-03-21 14:07:24 +0800

    remove sdk

--- 3ds Max 2010 SDK/maxsdk/howto/3dsmaxPluginWizard/Templates/1033/userDefaults_template.js
+++
@@ -1,3 +0,0 @@
-window.external.AddSymbol("SDKPATH", "[!output SDKPATH_PRINTABLE]");
-window.external.AddSymbol("PLGPATH", "[!output PLGPATH_PRINTABLE]");
-window.external.AddSymbol("EXEPATH", "[!output EXEPATH_PRINTABLE]");
